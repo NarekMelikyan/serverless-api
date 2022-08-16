09088bd95e8da139ec288662a79346a29f388d61
NarekMelikyan <narekmel1995@gmail.com>
2022-08-16 23:02:33 +0400

    fix createStar body

--- api.js
+++ api.js
@@ -2,49 +2,50 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     region: 'us-east-1'
 });
-const helpers = require('./helpers')
+const helpers = require('./helpers');
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dynamoTableName = 'stars-table-dev';
 
 const createStar = async (event) => {
     try {
-        return buildResponse(201, event)
+        const params = {
+            TableName: dynamoTableName,
+            Item: JSON.parse(event.body)
+        };
 
-        // const params = {
-        //     TableName: dynamoTableName,
-        //     Item: event.body
-        // }
-        //
-        // const star = await dynamodb.put(params).promise()
+        const star = await dynamodb.put(params).promise();
 
+        return buildResponse(201, star);
     } catch (e) {
-        return buildResponse(400, e)
+        return buildResponse(400, e);
     }
 };
 
 const getAllStars = async (event) => {
     try {
         let allMovieStars;
+
         if(event.queryStringParameters) {
             allMovieStars = await dynamodb.scan({
                 FilterExpression: helpers.collectFilterExpression(event.queryStringParameters),
                 ExpressionAttributeNames: helpers.collectExpressionAttributeNames(event.queryStringParameters),
                 ExpressionAttributeValues: helpers.collectExpressionAttributeValues(event.queryStringParameters),
-                TableName: dynamoTableName
+                TableName: dynamoTableName,
             }).promise();
         } else {
             allMovieStars = await dynamodb.scan({
                 TableName: dynamoTableName,
             }).promise();
         }
+
         if (allMovieStars['Count'] === 0) {
-            return buildResponse(404, "There is no such person")
+            return buildResponse(404, "There is no such person");
         }
 
-        return buildResponse(200, allMovieStars)
+        return buildResponse(200, allMovieStars);
     } catch (e) {
-        return buildResponse(400, e)
+        return buildResponse(400, e);
     }
 };
 
@@ -56,7 +57,7 @@ const buildResponse = (statusCode, body) => {
         },
         body: JSON.stringify(body)
     }
-}
+};
 
 module.exports = {
     createStar,
